cbbf6d81b90513cec39022d8b00627dd9e5d604b
Dan Haughey <dan@greenlynx.org.uk>
2011-06-13 12:10:01 +0100

    Added copyright notice

--- src/pedalog.h
+++ src/pedalog.h
@@ -1,3 +1,24 @@
+/*
+   pedalog.h
+
+   Copyright (c) 2011 Dan Haughey
+
+   This file is part of libpedalog.
+
+   libpedalog is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   libpedalog is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with libpedalog.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _PEDALOG_H_
 #define _PEDALOG_H_
 
